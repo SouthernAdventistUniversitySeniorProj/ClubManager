793436171557093c5e46338ee96e61ccf4058c74
Qwynn Marcelle <qmarcelle@gmail.com>
2014-10-08 10:45:51 -0400

    added test@test.com username login is hello

--- clubProject/app/src/main/java/com/seniorproject/sauclubmanager/com/seniorproject/utilities/LoginScreen.java
+++ clubProject/app/src/main/java/com/seniorproject/sauclubmanager/com/seniorproject/utilities/LoginScreen.java
@@ -43,7 +43,7 @@ public class LoginScreen extends Activity implements LoaderCallbacks<Cursor>{
      * TODO: remove after connecting to a real authentication system.
      */
     private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "qmarcelle@gmail.com:hello","foo@example.com:hello", "bar@example.com:world"
+            "qmarcelle@gmail.com:hello","test@test.com:hello"
     };
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
