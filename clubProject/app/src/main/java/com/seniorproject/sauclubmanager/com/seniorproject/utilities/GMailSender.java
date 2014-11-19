bea39729f96d8313f3eefc0886051f3da7411890
duvonneb <duvonneb@gmail.com>
2014-11-19 05:28:25 -0500

    demo example 4 now last... everything is now pushed again.

--- clubProject/app/src/main/java/com/seniorproject/sauclubmanager/com/seniorproject/utilities/GMailSender.java
+++ clubProject/app/src/main/java/com/seniorproject/sauclubmanager/com/seniorproject/utilities/GMailSender.java
@@ -4,8 +4,6 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.security.AccessController;
-import java.security.Provider;
 import java.security.Security;
 import java.util.Properties;
 
@@ -28,7 +26,7 @@ public class GMailSender extends javax.mail.Authenticator {
     private Session session;
 
     static {
-        Security.addProvider(new JSSEProvider().this);
+        Security.addProvider(new JSSEProvider());
     }
 
     public GMailSender(String user, String password) {
@@ -106,25 +104,6 @@ public class GMailSender extends javax.mail.Authenticator {
 
         public OutputStream getOutputStream() throws IOException {
             throw new IOException("Not Supported");
-        }
-    }
-
-    public final class JSSEProvider extends Provider {
-
-        public JSSEProvider() {
-            super("HarmonyJSSE", 1.0, "Harmony JSSE Provider");
-            AccessController.doPrivileged(new java.security.PrivilegedAction<Void>() {
-                public Void run() {
-                    put("SSLContext.TLS",
-                            "org.apache.harmony.xnet.provider.jsse.SSLContextImpl");
-                    put("Alg.Alias.SSLContext.TLSv1", "TLS");
-                    put("KeyManagerFactory.X509",
-                            "org.apache.harmony.xnet.provider.jsse.KeyManagerFactoryImpl");
-                    put("TrustManagerFactory.X509",
-                            "org.apache.harmony.xnet.provider.jsse.TrustManagerFactoryImpl");
-                    return null;
-                }
-            });
         }
     }
 }
